f624245f3f696dd0e4b8d43404d230fcdf1547a3
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-05-04 11:11:09 +0200

    Implement pieces with 7 combinations
    One piece has 4 blocks. The piece moves its blocks. When a piece
    vertically collides, its blocks get appended to the placed_blocks array

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ pygame.init()
 ROWS = 20
 COLS = 10
 CELL_SIZE = 35
+SCREEN_MIDDLE = COLS / 2
 SCREEN_WIDTH = COLS * CELL_SIZE
 SCREEN_HEIGHT = ROWS * CELL_SIZE
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,64 +22,133 @@ pygame.display.set_icon(icon)
 
 
 class Block:
-    def __init__(self):
-        self.previous_pivot = None
-        self.pivot = Vector2(COLS / 2, 15)
+    def __init__(self, pos):
+        self.previous_pos = None
+        self.pos = pos
 
     def draw(self):
-        x = self.pivot.x * CELL_SIZE
-        y = self.pivot.y * CELL_SIZE
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
         rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
         pygame.draw.rect(screen, (0, 0, 255), rect)
 
     def move_down(self):
-        self.pivot.y += 1
-        self.previous_pivot = Vector2(self.pivot.x, self.pivot.y - 1)
+        self.pos.y += 1
+        self.previous_pos = Vector2(self.pos.x, self.pos.y - 1)
 
     def move_left(self):
-        self.pivot.x -= 1
-        self.previous_pivot = Vector2(self.pivot.x + 1, self.pivot.y)
+        self.pos.x -= 1
+        self.previous_pos = Vector2(self.pos.x + 1, self.pos.y)
 
     def move_right(self):
-        self.pivot.x += 1
-        self.previous_pivot = Vector2(self.pivot.x - 1, self.pivot.y)
+        self.pos.x += 1
+        self.previous_pos = Vector2(self.pos.x - 1, self.pos.y)
+
+
+class Piece:
+    BLOCK_COMBINATIONS = [
+        # I
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE, 1),
+         (SCREEN_MIDDLE, 2),
+         (SCREEN_MIDDLE, 3)],
+
+        # O
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE + 1, 0),
+         (SCREEN_MIDDLE, 1),
+         (SCREEN_MIDDLE + 1, 1)],
+
+        # T
+        [(SCREEN_MIDDLE - 1, 0), (SCREEN_MIDDLE, 0),
+         (SCREEN_MIDDLE + 1, 0),
+         (SCREEN_MIDDLE, 1)],
+
+        # S
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE + 1, 0),
+         (SCREEN_MIDDLE, 1),
+         (SCREEN_MIDDLE - 1, 1)],
+
+        # Z
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE + 1, 0),
+         (SCREEN_MIDDLE, -1),
+         (SCREEN_MIDDLE - 1, -1)],
+
+        # J
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE, 1),
+         (SCREEN_MIDDLE + 1, 1),
+         (SCREEN_MIDDLE + 2, 1)],
+
+        # L
+        [(SCREEN_MIDDLE, 0), (SCREEN_MIDDLE, 1),
+         (SCREEN_MIDDLE - 1, 1),
+         (SCREEN_MIDDLE - 2, 1)]
+    ]
+
+    def __init__(self):
+        self.blocks = []
+        self.randomize_blocks()
+
+    def randomize_blocks(self):
+        positions = random.choice(self.BLOCK_COMBINATIONS)
+        blocks = []
+        [blocks.append(Block(Vector2(position))) for position in positions]
+        self.blocks = blocks
+
+    def draw(self):
+        for block in self.blocks:
+            block.draw()
+
+    def move_left(self):
+        for block in self.blocks:
+            block.move_left()
+
+    def move_right(self):
+        for block in self.blocks:
+            block.move_right()
+
+    def move_down(self):
+        for block in self.blocks:
+            block.move_down()
+
+    def set_previous_pos(self):
+        for block in self.blocks:
+            block.pos = block.previous_pos
 
 
 class Game:
     def __init__(self):
         self.placed_blocks = []
-        self.block = Block()
+        self.piece = Piece()
 
     def draw_placed_blocks(self):
-        for block in self.placed_blocks:
-            block.draw()
+        [block.draw() for block in self.placed_blocks]
 
-    def new_block(self):
-        self.block = Block()
+    def new_piece(self):
+        self.piece = Piece()
 
     def handle_horizontal_collision(self):
-        placed_pivots = [block.pivot for block in self.placed_blocks]
-        if self.block.pivot.x < 0 or self.block.pivot.x > COLS - 1 or self.block.pivot in placed_pivots:
-            self.block.pivot = self.block.previous_pivot
+        placed_positions = [block.pos for block in self.placed_blocks]
+        for piece_block in self.piece.blocks:
+            if piece_block.pos.x < 0 or piece_block.pos.x > COLS - 1 or piece_block.pos in placed_positions:
+                self.piece.set_previous_pos()
 
     def handle_vertical_collision(self):
-        placed_pivots = [block.pivot for block in self.placed_blocks]
-        if self.block.pivot.y >= ROWS or self.block.pivot in placed_pivots:
-            self.block.pivot = self.block.previous_pivot
-            self.placed_blocks.append(self.block)
-            self.new_block()
+        placed_positions = [block.pos for block in self.placed_blocks]
+        for piece_block in self.piece.blocks:
+            if piece_block.pos.y >= ROWS or piece_block.pos in placed_positions:
+                self.piece.set_previous_pos()
+                [self.placed_blocks.append(block) for block in self.piece.blocks]
+                self.new_piece()
 
     def handle_clear(self):
         if self.placed_blocks:
             lowermost_row_blocks_count = 0
             for block in self.placed_blocks:
-                if block.pivot.y == ROWS - 1:
+                if block.pos.y == ROWS - 1:
                     lowermost_row_blocks_count += 1  # If block is in the lowermost row, increment by 1
-
                 if lowermost_row_blocks_count == COLS:  # If lowermost row is full
                     i = 0
                     while i in range(len(self.placed_blocks)):  # Delete all blocks from the lowermost row
-                        if self.placed_blocks[i].pivot.y == ROWS - 1:
+                        if self.placed_blocks[i].pos.y == ROWS - 1:
                             del self.placed_blocks[i]
                         else:
                             i += 1
@@ -105,21 +175,20 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                game.block.move_left()
+                game.piece.move_left()
                 game.handle_horizontal_collision()
             elif event.key == pygame.K_RIGHT:
-                game.block.move_right()
+                game.piece.move_right()
                 game.handle_horizontal_collision()
-            elif event.key == pygame.K_DOWN:
-                game.block.move_down()
-                game.handle_vertical_collision()
-            elif event.key == pygame.K_SPACE:
-                for blokign in game.placed_blocks:
-                    print(blokign.pivot.x, blokign.pivot.y)
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_DOWN]:
+        game.piece.move_down()
+        game.handle_vertical_collision()
 
     game.handle_clear()
 
     game.draw_placed_blocks()
-    game.block.draw()
+    game.piece.draw()
 
     pygame.display.update()
